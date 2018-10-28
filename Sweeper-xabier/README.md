8a14284748eb9254a4498d31793be2860ae6f9b4
Voidtekvla <43203570+Voidtekvla@users.noreply.github.com>
2018-10-28 21:28:47 +0100

    Add files via upload
    Manejo de múltiples capas (cambios en room, overworld y cambios simples de posicion en player y dummy)

---
+++ Sweeper-xabier/README.md
@@ -0,0 +1,54 @@
+# Sweeper
+
+[Gameplay](#gameplay)  
+[Temática](#tematica)  
+[Referencias](#referencias)  
+[Integrantes](#integrantes)  
+
+## Gameplay
+La partida consta de un mínimo de dos jugadores que explorarán el área jugable a través de una vista cenital. Los jugadores pueden desplazarse, atacar, recoger objetos, y accionar palancas y otros mecanismos, así como recibir daño por parte de ataques enemigos o del ataque descuidado de otro jugador.
+
+El área jugable es un conjunto de salas interconectadas y agrupadas en múltiples zonas. Para avanzar por la zona, los jugadores deben desbloquear u obtener herramientas o poderes que permitan sortear ciertos obstáculos. Estos obstáculos bloquean el paso a varias secciones del juego que son inaccesibles hasta obtener dichas herramientas, efectivamente ofreciendo una experiencia más o menos lineal en un entorno que hay que explorar. Existe una zona central que, además de suponer un desafío similar al de las otras zonas, permite navegar entre las demás zonas del juego.
+
+Los enemigos a eliminar se dividen en dos categorías: débiles y fuertes. Los enemigos débiles aparecen aleatoriamente en salas donde no haya jugadores y hay una cantidad indefinida de ellos, mientras que los enemigos fuertes sólo aparecen una vez por partida y es necesario derrotarlos para avanzar en el juego. Eliminar a un enemigo fuerte hace que ciertos tipos de enemigos débiles también dejen de aparecer.
+
+Los enemigos fuertes, así como ciertos obstáculos, exigen la cooperación de todos los jugadores en la partida. Los jugadores pueden ir por separado, pero deben estar en la misma zona en todo momento. Para pasar de una zona a otra es necesario que todos los jugadores estén juntos.
+
+Las herramientas y las mejoras se obtienen principalmente de dos formas: Encontradas en un lugar predeterminado, o soltadas por un enemigo derrotado.
+
+## Temática
+
+La ambientación del juego es urbana y cotidiana. La partida transcurre en una ciudad que ha caído víctima de un suceso paranormal indefinido. Los enemigos a eliminar son elementos del entorno alterados por dicho suceso. Las zonas a explorar son lugares habituales de una ciudad, por ejemplo: un supermercado, una estación de metro, unas alcantarillas, etc. La zona central sería la plaza principal de la ciudad.
+
+## Referencias
+![Graveyard Keeper](https://guides.gamepressure.com/graveyard-keeper/gfx/word/436349125.jpg "Graveyard Keeper")  
+El ángulo de visión sería similar a Graveyard Keeper  
+
+![Punch Club](https://images.g2a.com/newlayout/470x470/1x1x0/c5e9522e979b/5912e6bcae653aba4e0db542 "Punch Club")  
+La estética estaría inspirada en Punch Club  
+
+![Metroid Samus Returns](https://vignette.wikia.nocookie.net/metroid/images/1/15/Metroid_Samus_Returns_area_2_map.png "Metroid Samus Returns")  
+El planteamiento del diseño de nivel para las distintas zonas del juego estarían basadas en juegos como Metroid o Hollow Knight  
+
+![Mario & Luigi: Superstar Saga](https://i.ytimg.com/vi/FyB2U2lIaLI/hqdefault.jpg "Mario & Luigi: Superstar Saga")  
+Los enemigos más fuertes y algunos obstáculos requerirán cooperación y compenetración por parte de los jugadores. Estarían planteados de forma similar a la saga Mario & Luigi, pero en multijugador y a tiempo real.  
+
+## Integrantes  
+
+1. **Nombre y Apellidos:** Pablo Alconchel Palma  
+   **Correo universidad:** p.alconchel.2016@alumnos.urjc.es  
+   **Cuenta Github:** https://github.com/Poxterx  
+   
+2. **Nombre y Apellidos:** Raquel Pizarro Expósito  
+   **Correo universidad:** r.pizarro.2016@alumnos.urjc.es   
+   **Cuenta Github:** https://github.com/RaquelURJC  
+   
+3. **Nombre y Apellidos:** Carlos González Cruz  
+   **Correo universidad:** c.gonzalezcr.2016@alumnos.urjc.es  
+   **Cuenta Github:** https://github.com/CarlosGonzalezCruz  
+     
+4. **Nombre y Apellidos:** Xabier Villanueva Loureiro  
+   **Correo universidad:** x.villanueva.2016@alumnos.urjc.es  
+   **Cuenta Github:** https://github.com/Voidtekvla  
+  
+**Link Trello:** https://trello.com/b/bTIkqQ83/game-de-jer
---
+++ Sweeper-xabier/typescript/room.ts
@@ -0,0 +1,198 @@
+type RoomConfig = {
+    /**
+     * Nombre o dirección de la imagen que define el aspecto de los tiles
+     * (partiendo de assets/tilesets)
+     */
+    tileset :string,
+    /**
+     * Nombre o dirección del archivo que contiene el trazado del mapa
+     * (partiendo de assets/maps)
+     */
+    tilemap :string
+}
+
+class Room {
+    /**
+     * Nombre que identifica a esta sala
+     */
+    public name :string;
+    /**
+     * Escena asociada con esta sala
+     */
+    public scene :Phaser.Scene;
+    /**
+     * Dimensiones en píxeles de esta sala. IMPORTANTE: ES POSIBLE QUE LA MEDIDA DE ESTA VARIABLE PASE DE
+     * PÍXELES A TILES EN FUTURAS VERSIONES.
+     */
+    public size :Phaser.Math.Vector2;
+    /**
+     * Array de capas que contienen las colisiones de esta sala
+     */
+    public colliderLayers :Phaser.Tilemaps.DynamicTilemapLayer[];
+    /**
+     * Objeto de configuración con el que se inicializó la sala
+     */
+    private config :RoomConfig;
+    /**
+     * Trazado del mapa cargado desde el archivo especificado en config.tilemap
+     */
+    private map :Phaser.Tilemaps.Tilemap;
+    /**
+     * Imagen que define el aspecto de los tiles de esta sala
+     */
+    private tileset :Phaser.Tilemaps.Tileset;
+    /**
+     * Capas de la sala según están creadas en Tiled
+     */
+    private layers :Phaser.Tilemaps.DynamicTilemapLayer[];
+
+    constructor(name :string, config :RoomConfig) {
+        this.name = name;
+        this.config = config;
+        this.colliderLayers = [];
+    }
+
+    /**
+     * Devuelve el tile de Phaser que se encuentra en la posición indicada en la capa de colisiones
+     * de esta sala
+     * @param tile La posición del tile que queremos
+     */
+    public getColliderTileAt(tile :Vector2) {
+        for (let l = 0; l < this.colliderLayers.length; l++) {
+            if(this.colliderLayers[l].getTileAt(tile.x, tile.y, true).index != -1)
+                return this.colliderLayers[l].getTileAt(tile.x, tile.y, true);
+        }
+        return this.colliderLayers[this.colliderLayers.length - 1].getTileAt(tile.x, tile.y, true);
+    }
+
+    /**
+     * Carga los recursos necesarios para esta sala
+     */
+    preload() {
+        // Cargamos el tileset y el mapa
+        this.scene.load.tilemapTiledJSON(this.name + "@tilemap", "assets/maps/"+this.config.tilemap);
+        this.scene.load.image(this.name + "@tileset", "assets/tilesets/"+this.config.tileset);
+    }
+
+    /**
+     * Inicializa los recursos cargados con preload()
+     */
+    create() {
+        // Creamos el tileset y el mapa
+        this.map = this.scene.make.tilemap({key: this.name + "@tilemap"});
+        this.tileset = this.map.addTilesetImage(this.map.tilesets[0].name, this.name + "@tileset");
+
+        // El tamaño de la sala será el tamaño del mapa
+        this.size = new Phaser.Math.Vector2(this.map.widthInPixels, this.map.heightInPixels);
+
+        // Variables que almacenan los distintos tipos de capa
+        var water:Phaser.Tilemaps.LayerData;
+        var floor:Phaser.Tilemaps.LayerData;
+        var wall:Phaser.Tilemaps.LayerData;
+        var lights:Phaser.Tilemaps.LayerData;
+
+        // Guardamos en un array todas las capas guardadas en el archivo tilemap
+        this.layers = [];
+        for(let layer of this.map.layers) {
+            //this.layers.push(this.map.createDynamicLayer(layer.name, this.tileset, 0, 0));
+            switch (layer.name) {
+                case "agua":
+                    water = layer;
+                    break;
+                case "suelo":
+                    floor = layer;
+                    break;
+                case "paredes":
+                    wall = layer;
+                    break;
+                case "Luz":
+                    lights = layer;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        // La segunda capa en el archivo contendrá el suelo
+        if(floor){
+            this.layers.push(this.map.createDynamicLayer(floor.name, this.tileset, 0, 0));
+        }
+
+        // La tercera capa en el archivo es la que debe contener las paredes. Las colisiones y el
+        // renderizado que se procesan más adelante parten del supuesto de que esta capa existe. Si
+        // no es así, esta función ya no tiene nada más que hacer.
+        if(wall){
+            this.layers.push(this.map.createDynamicLayer(wall.name, this.tileset, 0, 0));
+            // En esta implementación, los tiles sólidos son los que en Tiled tienen una propiedad
+            // personalizada, de tipo booleano, llamada "Solid". Esta es la capa que indicamos a las otras clases
+            // que representa la máscara de colisiones de esta sala.
+            this.layers[this.layers.length - 1].setCollisionByProperty({Solid: true});
+            this.colliderLayers.push(this.layers[this.layers.length - 1]);
+
+            // Ya no necesitamos renderizar la capa de colisiones, así que la desactivamos para obtener
+            // un poco de rendimiento extra
+            //this.layers[this.layers.length - 1].visible = false;
+
+            // Teniendo ya cargados los tiles de la pared, vamos a crear múltiples capas para renderizar
+            // los mismos. Esto es necesario para que la renderización de las paredes se ordenen, al igual
+            // que el resto de elementos del juego, a través del nivel de profundidad. Sin este procedimiento
+            // se producen bugs visuales que son relativamente comunes y fáciles de provocar y que pueden
+            // romper la inmersión del jugador.
+
+            // Cada fila de tiles de pared tiene su propia capa, debido a que la implementación de tilemaps
+            // de Phaser sólo puede asignarle un valor de profundidad individual a, como mínimo, una capa
+            for(let row = 0; row < this.map.height; row++) {
+                // Creamos la capa para esta fila. El guión bajo inicial es una convención que *no* vamos a
+                // utilizar para nombrar capas en Tiled.
+                this.layers.push(this.map.createBlankDynamicLayer("_WallLayer"+row, this.tileset, 0, 0));
+                // La capa actual es la que acabamos de añadir al array, es decir la última
+                var current = this.layers.length - 1;
+                // Ahora pasamos por cada tile de esta fila
+                for(let column = 0; column < this.map.width; column++) {
+
+                    var currentTile = this.layers[this.layers.length - 1].getTileAt(column, row);
+
+                    // Si el tile está marcado como Vertical es porque en realidad pertenece a la fila
+                    // superior, aunque debido a la perspectiva tuviera que colocarse en esta.
+                    // Si es el caso, lo ignoramos.
+                    // También es posible que el tile no exista, por eso antes de comprobar nada más hay
+                    // que comprobar que sí existe, de ahí el "currentTile &&"
+                    if(currentTile && !currentTile.properties.Vertical) {
+                        // Y si no es el caso, lo añadimos a la capa 
+                        this.layers[current].putTileAt(currentTile, column, row);
+                    }
+
+                    // ¿Qué sucede con el tile de la fila inferior? Si está marcado como Vertical es
+                    // que pertenece a esta fila
+                    currentTile = this.layers[this.layers.length - 1].getTileAt(column, row + 1);        
+                    if(currentTile && currentTile.properties.Vertical) {
+                        // Si está marcado como Vertical entonces lo añadimos a esta fila
+                        this.layers[current].putTileAt(currentTile, column, row + 1);
+                    }
+                }
+                // Le asignamos a la capa la profundidad correspondiente a su posición. El factor "(row + 2)"
+                // está pensado para que esta asignación de profundidad tenga en cuenta que la pared está
+                // elevada. Si pusiéramos "row" a secas, quedaría visualmente a la altura del suelo.
+                this.layers[current].depth = (row + 2) * this.map.tileHeight;
+            }
+
+        }
+        
+        // La cuarta capa en el archivo es la que contiene las luces.
+        // Nos aseguramos de asignarle una profundidad superior a la de las paredes,
+        // en donde están colocadas.
+        if(lights){
+            this.layers.push(this.map.createDynamicLayer(lights.name, this.tileset, 0, 0));
+            this.layers[this.layers.length - 1].depth = this.layers[this.layers.length - 2].depth + 1;
+        }
+        
+        // La primera capa en el archivo es la que contiene el agua. Las colisiones y el
+        // renderizado que se procesan más adelante parten del supuesto de que esta capa existe. Si
+        // no es así, esta función ya no tiene nada más que hacer.
+        if(water){
+            this.layers.push(this.map.createDynamicLayer(water.name, this.tileset, 0, 0));
+            this.layers[this.layers.length - 1].setCollisionByProperty({Solid: true});
+            this.colliderLayers.push(this.layers[this.layers.length - 1]);
+        }
+    }
+}
